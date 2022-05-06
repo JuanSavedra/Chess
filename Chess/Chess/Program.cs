1d968a21398519e18bcacd600cf0771a5f12707a
Juan Savedra <juansavedra827@gmail.com>
2022-05-06 13:27:45 -0300

    class Position

--- Chess/Chess/Program.cs
+++ Chess/Chess/Program.cs
@@ -1,7 +1,10 @@
-﻿namespace Chess {
+﻿using Chess.Tabuleiro;
+
+namespace Chess {
     class Program {
         static void Main(string[] args) {
-            
+            Position p = new Position(3, 4);
+            Console.WriteLine(p);
         }
     }
 }
---
+++ Chess/Chess/Tabuleiro/Position.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Tabuleiro {
+    class Position {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position() { }
+
+        public Position(int line, int column) { 
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString() {
+            return $"Line: {Line}, " +
+                $"Column: {Column}";
+        }
+    }
+}
